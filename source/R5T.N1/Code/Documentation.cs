e87365db5d021c6b6e521aaddb806ca3739e4030
davidcoats <david@getsafetycone.com>
2024-01-18 01:20:44 -0800

    Interim changes.

--- source/R5T.N1/Code/Documentation.cs
+++ source/R5T.N1/Code/Documentation.cs
@@ -1,11 +1,20 @@
 using System;
 
+using R5T.T0156;
+
 
 namespace R5T.N1
 {
-	/// <summary>
-	/// The location anonymous namespace.
-	/// </summary>
+    /// <summary>
+    /// The location-anonymous namespace.
+    /// </summary>
+    /// <remarks>
+    /// Location-anonymous types are those types for which their project location does not matter.
+    /// Generally, there should only be one of each of these types.
+    /// However, it is possible to have more than one of these types (same name, in different projects),
+    /// but consumers of either type will be annoyed by having the alias their type names if both libraries are ever dependencies.
+    /// </remarks>
+    [DocumentationMarker]
 	public static class Documentation
 	{
 	}
--- source/R5T.N1/Project Plan.md
+++ source/R5T.N1/Project Plan.md
@@ -1,2 +1,8 @@
 # R5T.N1
-The location anonymous namespace.
+The location-anonymous namespace.
+
+This library should contain no code, it is just a placeholder for the location-anonymous concept.
+
+This library contains only a documentation class explaining the concept in its XML documentation comment.
+
+This library allows other libraries to reference it when they want to reference the location-anonymous concept.
